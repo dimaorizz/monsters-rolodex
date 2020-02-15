83244b60a20ef31a69cccf5028e3184d42b15882
Orizz <dimaorizz@gmail.com>
2020-02-15 13:51:07 +0200

    Updates

---
+++ precache-manifest.c668bca2cd3ed56c06847d254a6f3246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3d6de8668a95bc854fd7b53c9a21cc",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ccfa824d0e837860ded0",
+    "url": "/monsters-rolodex/static/css/main.3b24729c.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccfa824d0e837860ded0",
+    "url": "/monsters-rolodex/static/js/main.83e1ac04.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
